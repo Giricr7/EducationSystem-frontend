ff4e2fa49362f3411aa2d913ce86aa0e86f9f9d7
Giriprasath <giriarjun1995@gmail.com>
2022-03-03 19:30:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/';
+axios.defaults.baseURL = 'https://educationsystem-backend.herokuapp.com/';
 
 function App() {
 	return (
